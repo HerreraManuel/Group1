7aba042f348b5986bba57c0504ff92061517d6c1
jerodnoonercsus <jerodnooner@csus.edu>
2018-11-04 16:12:59 -0700

    dashboard additions

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -2,9 +2,26 @@ import javax.swing.*;
 
 public class Dashboard extends JFrame {
 
-    private JFrame window;
+//    private JFrame window;
     private JPanel leftside;
     private JPanel rightside;
+
+    private JTextArea Display;
+    private JScrollPane scroll;
+    private JPanel totals;
+
+    private JLabel instructions;
+    private JTextField urlinput;
+    private JPanel totbuttons;
+    private JCheckBox check1;
+    private JCheckBox check2;
+    private JCheckBox check3;
+    private JCheckBox check4;
+    private JCheckBox check5;
+    private JTextField additional;
+    private JButton enter;
+
+
     private boolean initialized = false;
     private Actions actions = new Actions();
 
@@ -28,8 +45,58 @@ public class Dashboard extends JFrame {
             return;
         }
         initialized  = true;
+        this.setLayout(new FlowLayout());
         this.setSize(500, 400);
         //TODO: add in your layers and everything else in here
+        check1 = new JCheckBox("Characters");
+        check1.addActionListener(this);
+
+        check2 = new JCheckBox("Words");
+        check2.addActionListener(this);
+
+        check3 = new JCheckBox("Lines");
+        check3.addActionListener(this);
+        
+        check4 = new JCheckBox("Source Lines");
+        check4.addActionListener(this);
+
+        check5 = new JCheckBox("CommentLines");
+        check5.addActionListener(this);
+
+        totbuttons.setLayout(new BoxLayout(totbuttons, BoxLayout.Y_AXIS));
+        totbuttons.add(check1);
+        totbuttons.add(check2);
+        totbuttons.add(check3);
+        totbuttons.add(check4);
+        totbuttons.add(check5);
+
+        leftside.setLayout(new BoxLayout(leftside, BoxLayout.Y_AXIS));
+        instructions = new JLabel("Enter Github URL");
+        leftside.add(instructions);
+
+        urlinput = new JTextField(60);
+        urlinput.addActionListener(this);
+        leftside.add(urlinput);
+
+        leftside.add(totbuttons);
+
+        enter = new JButton("Enter");
+        enter.addActionListener(this);
+        leftside.add(enter);
+
+        this.add(leftside);
+
+        Display = new JTextArea();
+        Display.setEditable(false);
+        scroll = new JScrollPane(Display);
+        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        rightside.add(scroll);
+        rightside.add(Display);
+        totals = new JTextArea();
+        rightside.add(totals);
+
+        this.add(rightside);
     }
 
     private void initializeEvents(){
