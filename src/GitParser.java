013c577e974bc381e45e0f8ba771337ee3baf5ca
Jenzel Arevalo <jenzelarevalo@csus.edu>
2018-11-05 05:52:48 -0800

    Created GitParser class for retrieving GitHub repositories

---
+++ src/GitParser.java
@@ -0,0 +1,12 @@
+/*
+Programmer(s): Jenzel Arevalo
+
+Class Description: GitParser utilizes the JGit external library to retrieve
+and process GitHub repositories.
+*/
+
+//TODO: Unit Under Test
+public class GitParser
+{
+
+}
