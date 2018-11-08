f1e538085c178b645a4a05c003868bd99cff9115
JerodNooner <43176857+JerodNooner@users.noreply.github.com>
2018-11-08 14:45:15 -0800

    Update Dashboard.java
    working on it

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -13,18 +13,13 @@ public class Dashboard extends JFrame {
     private JScrollPane scroll;
     private JTextArea totals;
 
-    private JLabel githubUrlInput;
-    private JTextField urlinput;
-    private JLabel additiionalLabel;
-    private JTextField additionalText;
-    private JCheckBox check1, check2, check3, check4, check5, enter, clear;
-    private JTextArea additional;
-  	private String complete url;
+    private JLabel githubUrlInput, additionalInput;
+    private JTextField urlinput, additionalText;
+    private JCheckBox check1, check2, check3, check4, check5;
+    private JButton enter, clear;
+  	private String completeUrl;
   	private boolean chars, words, lines , Sloc ,Cloc;
-
-
     private boolean initialized = false;
-    private Actions actions = new Actions();
 
     public static void main (String args[]){
         new Dashboard().setVisible(true);
@@ -53,8 +48,14 @@ public class Dashboard extends JFrame {
         leftside = new JPanel();
         leftside.setLayout(new BoxLayout(leftside , Y_AXIS));
         
+        rightside = new JPanel();
+        rightside.setLayout(new BoxLayout(rightside, X_AXIS);
+        
         urlPanel = new JPanel();
         urlPanel.setLayout(new BoxLayout(urlPanel , Y_AXIS));
+             
+        additionalPanel = new JPanel();
+        additionalPanel.setLayout(new BoxLayout(additionalPanel, Y_AXIS
         
         totbuttons = new JPanel();
         GridLayout grid = new GridLayout(2,3);
@@ -64,71 +65,81 @@ public class Dashboard extends JFrame {
         enterPanel.setLayout(new BoxLayout(enterPanel, X_AXIS);
         
         //TODO: add in your layers and everything else in here
-      
+    
+        githubUrlInput = new JLabel("Enter Github URL");
+        urlPanel.add(githubUrlInput);
+        urlinput = new JTextField(60);
+        urlinput.addActionListener(new ActionListener(){
+            public void actionPErformed(ActionEvent e){
+                completeUrl = urlInput.getText();
+            }
+        });
+        urlPanel.add(urlinput);
+        leftside.add(urlPanel);                                
+                             
+         
+                             
         check1 = new JCheckBox("Characters");
         check1.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
          	chars = true; 
         });
-
         check2 = new JCheckBox("Words");
         Actions listener2 = new Actions();
         check2.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
          	words = true; 
         });
-
         check3 = new JCheckBox("Lines");
         check3.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
          	lines = true; 
-        });
-        
+        });        
         check4 = new JCheckBox("Source Lines");
         check4.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e){
          	Sloc = true; 
         });
-
-      check5 = new JCheckBox("CommentLines");
-      check5.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-         	Cloc = true; 
-        }
-      });
-
+        check5 = new JCheckBox("CommentLines");
+        check5.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+         	    Cloc = true; 
+            }
+        });
         totbuttons.setLayout(new BoxLayout(totbuttons, BoxLayout.Y_AXIS));
         totbuttons.add(check1);
         totbuttons.add(check2);
         totbuttons.add(check3);
         totbuttons.add(check4);
         totbuttons.add(check5);
-
-        leftside.setLayout(new BoxLayout(leftside, BoxLayout.Y_AXIS));
-        instructions = new JLabel("Enter Github URL");
-        leftside.add(instructions);
-
-        urlinput = new JTextField(60);
-        Actions urlListener = new Actions();
-        urlinput.addActionListener(urlListener);
-        leftside.add(urlinput);
-
         leftside.add(totbuttons);
-
+        
         enter = new JButton("Enter");
-        Actions enterListener = new Actions();
       	enter.addActionListener(new ActionListener(){
         	public void actionPerformed(ActionEvent e){
         		displayData();		
         	}
       	});
+        enterPanel.add(enter);
         clear = new JButton("Clear");
         clear.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
-               
+               Display.setText(null);
+               totals.setText(null);
+               check1.setSelected(false);
+               chars = false;
+               check2.setSelected(false);
+               words = false;
+               check3.setSelected(false);
+               lines = false;
+               check4.setSelected(false);
+               Sloc = false;
+               check5.setSelected(false);
+               Cloc = false;
            }
         });
-        leftside.add(enter);
+        enterPanel.add(clear);
+        leftside.add(enterPanel);
 
         window.add(leftside);
 
@@ -137,8 +148,6 @@ public class Dashboard extends JFrame {
         scroll = new JScrollPane(Display);
         scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         
-        rightside = new JPanel();
-        rightside.setLayout(new BoxLayout(rightside, X_AXIS);
         rightside.add(scroll);
         totals = new JtextArea();
         rightside.add(totals);
@@ -146,7 +155,6 @@ public class Dashboard extends JFrame {
     }
                                  
     public void dispose() {
-        // TODO: Save settings
         super.dispose();
         System.exit(0);
     }
