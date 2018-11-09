33fa1f54bed4627d513184e9a946d1746b13037d
Jenzel Arevalo <jenzelarevalo@csus.edu>
2018-11-09 06:55:04 -0800

    + Found a way to download files via URL, need to implement   - NEEDS TO BE TESTED

--- src/GitParser/GitParser.java
+++ src/GitParser/GitParser.java
@@ -8,6 +8,9 @@ External Contributions:
 + JGit External Library
   http://www.eclipse.org/jgit/
 
++ How to download files via URL
+  https://www.baeldung.com/java-download-file
+
 + Examples for opening git repositories - designed by mobiblunt, maibin
   https://github.com/eugenp/tutorials/tree/master/JGit/src/main/java/com/baeldung/jgit
 */
@@ -23,6 +26,9 @@ import java.io.File;
 import java.io.IOException;
 
 //TODO: UNIT UNDER TEST - CURRENTLY UNSTABLE
+//1. Download a git repo via URL
+//2. Using JGit library, open the git repository
+
 public class GitParser
 {
     public static void main(String[] args) throws IOException, GitAPIException
@@ -68,4 +74,22 @@ public class GitParser
             return repository.getDirectory();
         }
     }
+
+    //This will allow us to download a file via URL
+    /*
+    try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
+    FileOutputStream fileOutputStream new FileOutputStream(FILE_NAME))
+    {
+        byte dataBuffer[] = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1)
+        {
+            fileOutputStream.write(dataBuffer, 0, bytesRead);
+        }
+    }
+    catch(IOException e)
+    {
+        // handle exception
+    }
+    */
 }
