bf827c21c99d39967000d650f96c63ec85acc8a5
Jenzel Arevalo <jenzelarevalo.github@gmail.com>
2018-11-11 20:46:25 -0800

    Genericized GitParser_TestMain

--- src/GitParser/GitParser_TestMain.java
+++ src/GitParser/GitParser_TestMain.java
@@ -1,18 +1,28 @@
 /***********************************************************************************
- * Programmer(s): Jenzel Arevalo
- *
- * Class: GitParser_TestMain
- *
- * Class Description:
- * Tests to see if Downloader class works.
- *
- * Bugs/Anomalies:
- * + TODO [MODERATE-PRIORITY]: Patch OS Path Download Compatibility
- *   Given its current state, Windows Users are able to download files and not
- *   Linux or MacOS users. This is most likely due to the way in which the string
- *   in the File constructor is inputted for -nix based Systems. The solution may
- *   be to rely on a Java library that can be utilized to navigate the system's
- *   directory to determine what operating system the program is running on.
+  Programmer(s): Jenzel Arevalo
+
+  Class: GitParser_TestMain
+
+  Class Description:
+  Tests to see if Downloader class works.
+
+  Notes:
+
+    Given its current state, Windows, Linux, and MacOS users are able to download
+    files to their system.
+
+    It should be noted that the path to the output file differs for Windows
+    and -nix based systems.
+
+    For Windows...
+    The output file path should look like:
+      C:\\some\\path\\for\\output\\file
+
+    For Linux and MacOS...
+    The output file path should look like:
+      /home/username/path/for/output/file
+
+    Notice the double back-slash for the Windows file path.
  **********************************************************************************/
 
 package GitParser;
@@ -25,13 +35,13 @@ public class GitParser_TestMain
     {
         String link = "https://pdfs.semanticscholar.org/efb2/58d7812128aa19709520b1a567da98227cc5.pdf"; //TEST: "A Concise Introduction to Software Engineering" PDF file
 
-        /* TODO: Windows Users - Please specify the path AND name of the pdf file you would like to use
-        *  For instance, Zoran David's file will be downloaded here:
-        *  File out = new File("C:\\Users\\Zoran David\\Desktop\\Java The Complete.pdf");
-        */
+        /* Example for Windows based Systems */
+        //File out = new File("C:\\Users\\Zoran David\\Desktop\\Java The Complete.pdf")
 
-        File out = new File(""); //TODO: type in path name here
+        /* Example for -nix based Systems */
+        //File out = new File("/home/filipinoy95/Downloads/A Concise Introduction to Software Engineering.pdf");
 
+        File out = new File (" "); //TODO: Enter a valid path and file name with extension
 
         new Thread(new Downloader(link, out)).start();
     }
