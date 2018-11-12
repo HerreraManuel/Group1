0f77aa3f3a13bf78c8bd00e7e89e03af271db732
Jenzel Arevalo <jenzelarevalo.github@gmail.com>
2018-11-11 16:27:27 -0800

    Created a class description header for Downloader and Downloader_TestMain

--- src/GitParser/Downloader_TestMain.java
+++ src/GitParser/Downloader_TestMain.java
@@ -1,3 +1,20 @@
+/***********************************************************************************
+ * Programmer(s): Jenzel Arevalo
+ *
+ * Class: Downloader_TestMain
+ *
+ * Class Description:
+ * Tests to see if Downloader class works.
+ *
+ * Bugs/Anomalies:
+ * + TODO [MODERATE-PRIORITY]: Patch OS Path Download Compatibility
+ *   Given its current state, Windows Users are able to download files and not
+ *   Linux or MacOS users. This is most likely due to the way in which the string
+ *   in the File constructor is inputted for -nix based Systems. The solution may
+ *   be to rely on a Java library that can be utilized to navigate the system's
+ *   directory to determine what operating system the program is running on.
+ **********************************************************************************/
+
 package GitParser;
 
 import java.io.File;
@@ -7,13 +24,13 @@ public class Downloader_TestMain
     public static void main(String[] args)
     {
         String link = "https://pdfs.semanticscholar.org/efb2/58d7812128aa19709520b1a567da98227cc5.pdf"; //TEST: "A Concise Introduction to Software Engineering" PDF file
-        File out = new File("home/filipinoy95/Desktop/Test.pdf");
 
         /* TODO: Windows Users - Please specify the path AND name of the pdf file you would like to use
         *  For instance, Zoran David's file will be downloaded here:
         *  File out = new File("C:\\Users\\Zoran David\\Desktop\\Java The Complete.pdf");
-        * */
-        //File out = new File(""); //TODO: type in path name here
+        */
+
+        File out = new File(""); //TODO: type in path name here
 
 
         new Thread(new Downloader(link, out)).start();
