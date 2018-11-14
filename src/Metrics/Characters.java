e2ac14956ed92d534f34678122fbae1b8fd9bb31
Parambir <parambir29@gmail.com>
2018-11-14 11:00:03 -0800

    rough sketch of characters method

--- src/Metrics/Characters.java
+++ src/Metrics/Characters.java
@@ -1,10 +1,29 @@
 package Metrics;
+import java.io.*;
 
 public class Characters {
 
+String filename;
+int chars;
+int charTotal;
 
+    public void charaCount() {
+        try {
+            FileReader file = new FileReader(filename);
+            BufferedReader buffer = new BufferedReader(file);
 
-
-
+            while (buffer.read() != -1) {
+                chars++;
+            }
+            charTotal+=chars;
+            buffer.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file");
+        }
+        catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
 
 }
