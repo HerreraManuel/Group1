dd88441f07b80ae466f6c8964d283a7a2099e46d
jenzelarevalo <jenzelarevalo.github@gmail.com>
2018-11-13 21:09:55 -0800

    Test of GitCloner is successful, need more work to be done. Able to clone a full repository. Downloader class no longer be necessary.

--- src/GitParser/GitCloner.java
+++ src/GitParser/GitCloner.java
@@ -11,25 +11,22 @@
  * + How to Clone Git Repositories with JGit
  *   https://www.codeaffine.com/2015/11/30/jgit-clone-repository/
  **********************************************************************************/
-
 package GitParser;
 
 import java.io.File;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
-//TODO: Class is Unstable - Further development is needed
 public class GitCloner
 {
   public GitCloner()
-  {
-  }
+  {}
 
-  public void cloneRepository(String url, File workDirectory) throws GitAPIException
+  public void cloneRepository(String url, File outputFile) throws GitAPIException
   {
-      Git git = Git.cloneRepository()
-                    .setURI(url)
-                    .setDirectory(workDirectory) //TODO: Determine a place to store repository for specific OS, will save to a Linux
-                    .call();
+    Git git = Git.cloneRepository()
+            .setURI(url)
+            .setDirectory(outputFile)
+            .call();
   }
 }
