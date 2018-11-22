aed35371a51e2034a563d9c04a4224c7c35a198f
Manuel Herrera <mherrera@csus.edu>
2018-11-21 16:09:12 -0800

    Temporary source line output and file ext. detector.

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -1,15 +1,37 @@
 package Metrics;
 
+import java.io.File;
+
 public class SourceLine {
+
+    String fileExt;
 
     public void sourceline(String fileContent){
 
-        int sourceCount=0;
-        
-
-
+        int sourceCount = 0;
 
     }
+
+    // Tester for source lines output.
+    public static void Main(String[] args){
+
+    }
+
+    public boolean isJava(File tempFile){
+        fileExt = tempFile.getName().substring
+                (tempFile.getName().lastIndexOf("."));
+        return fileExt.equals(".java");
+    }
+
+    public boolean isCppOrC(File tempFile){
+        fileExt = tempFile.getName().substring
+                (tempFile.getName().lastIndexOf("."));
+        if (fileExt.equals(".h") || fileExt.equals(".cpp") ||
+                fileExt.equals(".h") || fileExt.equals("hpp"))
+            return true;
+        return false;
+    }
+
 
 
 }
