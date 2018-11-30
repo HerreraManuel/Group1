eb3e57253b298e013731e6a852c37592a8595b52
Jenzel Arevalo <jenzelarevalo.github@gmail.com>
2018-11-29 20:50:41 -0800

    Created a Result Class to store results of one File

---
+++ src/RepositoryAnalyzer/Result.java
@@ -0,0 +1,14 @@
+/***********************************************************************************************
+ Programmer(s): Jenzel Arevalo
+
+ Class Description:
+ Results class stores the results of the line count for each File
+
+ **********************************************************************************************/
+
+package RepositoryAnalyzer;
+
+public class Result
+{
+  //TODO: IMPLEMENT
+}
