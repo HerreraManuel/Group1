a36c4103adf46dbdf88129df25d441efa1debbba
Matthew Mendoza <melchormendoza@csus.edu>
2018-11-29 20:14:04 -0800

    Added method names to interface.

--- src/Retrievable.java
+++ src/Retrievable.java
@@ -1,3 +1,18 @@
 public interface Retrievable {
-    
+
+    boolean isURL(String path);
+    boolean isSuffix();
+
+    int getCharacterCount();
+    int getWordCount();
+    int getLineCount();
+    int getSourceCount();
+    int getCommentCount();
+
+    int getTotalCharacter();
+    int getTotalWord();
+    int getTotalLine();
+    int getTotalSource();
+    int getTotalComment();
+
 }
