9d3e8308c60d6cc9c059d55bbe967e4524f45f70
Jenzel Arevalo <jenzelarevalo.github@gmail.com>
2018-11-29 18:42:04 -0800

    Created custom exception classes for GitParser and RepositoryAnalyzer packages in an attempt to ID and insulate errors in their respective classes.

---
+++ src/GitParser/GitParserException.java
@@ -0,0 +1,14 @@
+/*************************************************************************************
+ Programmer(s): Jenzel Arevalo
+
+ Class Description:
+ Custom exception class that insulates and identifies errors that may occur from
+ the GitParser class
+ ************************************************************************************/
+
+package GitParser;
+
+public class GitParserException
+{
+  //TODO: IMPLEMENT
+}
---
+++ src/RepositoryAnalyzer/RepositoryAnalyzerException.java
@@ -0,0 +1,14 @@
+/*************************************************************************************
+ Programmer(s): Jenzel Arevalo
+
+ Class Description:
+ Custom exception class that insulates and identifies errors that may occur from
+ the RepositoryAnalyzer class
+ ************************************************************************************/
+
+package RepositoryAnalyzer;
+
+public class RepositoryAnalyzerException
+{
+    //TODO: IMPLEMENT
+}
