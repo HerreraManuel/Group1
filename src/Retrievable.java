a36f74326acf0b19aa6e45d24e96ede579471a6b
Matthew Mendoza <melchormendoza@csus.edu>
2018-12-01 14:49:34 -0800

    Checks if the user enters a valid suffix

--- src/Retrievable.java
+++ src/Retrievable.java
@@ -2,6 +2,12 @@ import GitParser.*;
 
 public class Grabber implements Retrievable {
 
+    public boolean isSuffix(String suffix)
+        if(suffix.contains(".java") || suffix.contains(".c") || suffix.contains(".h") || suffix.contains(".cpp") || suffix.contains(".hpp")){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 public interface Retrievable {
