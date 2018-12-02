17828bf50adeb169d9b1081525f190e024344491
HerreraManuel <mherrera@csus.edu>
2018-12-02 01:59:21 -0800

    SourceLine: fixed missing end bracket for the class

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -5,23 +5,25 @@ import java.util.Arrays;
 
 public class SourceLine {
 
-    public int sourceLine(String fileContent){
+    public int sourceLine(String fileContent) {
         ArrayList<String> lines;
         lines = new ArrayList<String>(Arrays.asList(fileContent.split("\n")));
         int sloc = 0, cloc = 0, bloc = 0;
         String singleLine = "";
-        for (int i = 0; i < lines.size(); i++){
+        for (int i = 0; i < lines.size(); i++) {
             singleLine = lines.get(i);
             if (singleLine.equals("") || singleLine.equals("{") || singleLine.equals("}")) bloc++;
             if (singleLine.startsWith("//")) cloc++;
             if (singleLine.contains("/*") && !singleLine.contains("*\\")) {
                 cloc++;
                 while (!singleLine.contains("*/")) {
-                    cloc++; sloc++; i++;
+                    cloc++;
+                    sloc++;
+                    i++;
                     singleLine = lines.get(i);
+                }
             }
         }
         return 0;
     }
-
 }
