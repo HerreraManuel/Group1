19bfb3c8ccbaa85d3a839e3a43ce75d7735eb4e0
HerreraManuel <mherrera@csus.edu>
2018-12-02 01:01:06 -0800

    SourceLine: Needed variables

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -8,6 +8,7 @@ public class SourceLine {
     public int sourceLine(String fileContent){
         ArrayList<String> lines;
         lines = new ArrayList<String>(Arrays.asList(fileContent.split("\n")));
+        int sloc = 0, cloc = 0, bloc = 0;
         return 0;
     }
 
