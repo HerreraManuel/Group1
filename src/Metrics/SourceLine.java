e14318a2c32a07fe92c804ee90bdcca15380169f
HerreraManuel <mherrera@csus.edu>
2018-12-02 00:36:55 -0800

    SourceLine: used fileContent inside asList()

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -7,7 +7,7 @@ public class SourceLine {
 
     public int sourceLine(String fileContent){
         ArrayList<String> lines;
-        lines = new ArrayList<String>(Arrays.asList());
+        lines = new ArrayList<String>(Arrays.asList(fileContent));
         String[] lineKeeper;
         lineKeeper = fileContent.split(("\\r?\\n"));
         return 0;
