014174445dab81d6b7e9aba1b8a960467f5051dd
HerreraManuel <mherrera@csus.edu>
2018-12-01 20:55:47 -0800

    CommentLine: removed Matthew's code

--- src/Metrics/CommentLine.java
+++ src/Metrics/CommentLine.java
@@ -25,60 +25,6 @@ public class CommentLine{
 
 }
 
-// Matthews Code Below
-/*
-public class CommentLine {
-
-    public void commentline(String fileContent){
-        try{
-            int commentCount = 0;
-            Scanner bk = new Scanner(fileContent);
-            String comment = "";
-            Boolean commentChecker;
-            while(bk.hasNextLine()){
-                comment = bk.nextLine();
-                for(int i = 0; i <= comment.length() - 1; i++){
-                    char com =  comment.charAt(i);
-                    if(i != comment.length() - 1){
-                        char comNext =  comment.charAt(i + 1);
-                        if(com == '/' && comNext == '/'){
-                            commentCount++;
-                        }
-
-                        if(com == '/' && comNext == '*'){
-                            commentCount++;
-                            commentChecker = true;
-                            while(commentChecker == true){
-                                while(bk.hasNextLine()){
-                                    if(commentChecker == false){
-                                        break;
-                                    }
-                                    String innerComment = bk.nextLine();
-                                    commentCount++;
-                                    for(int j = 0; j <= innerComment.length() - 1; j++){
-                                        char innerCom = innerComment.charAt(j);
-                                        if(j != innerComment.length() - 1){
-                                            char innerComNext = innerComment.charAt(j + 1);
-                                            if(innerCom == '*' && innerComNext == '/'){
-                                                commentChecker = false;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return commentCount;
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found");
-            return 0;
-        }
-    }
-}
-*/
 
 
 // Parambir's Code Below
