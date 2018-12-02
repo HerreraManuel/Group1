c5c2880e4875a218d67cb53bde51685a2added30
HerreraManuel <mherrera@csus.edu>
2018-12-02 00:19:29 -0800

    SourceLine: added bloc and cloc integers

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -5,7 +5,7 @@ public class SourceLine {
     public int sourceLine(String fileContent){
         String[] lineKeeper;
         lineKeeper = fileContent.split(("\\r?\\n"));
-        int sloc = 0;
+        int sloc = 0, cloc = 0, bloc = 0;
         for (int i = 0; i < lineKeeper.length; i++) {
 
         }
