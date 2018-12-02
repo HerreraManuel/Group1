61fc6976ef5b9c013ff6e7f5271dc7dfacb7a810
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:24:55 -0800

    MetricsException: removed simple error message

--- src/Metrics/MetricsException.java
+++ src/Metrics/MetricsException.java
@@ -3,7 +3,7 @@ package Metrics;
 //Safety measures for null/empty strings
 public class MetricsException extends Exception{
 
+
     public MetricsException(String exceptionMsg){
-        System.out.println("null or empty string");
     }
 }
