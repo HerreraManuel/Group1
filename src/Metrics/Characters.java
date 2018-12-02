2bd7c53a3b239e2c292600db13c92904418678a2
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:33:02 -0800

    Characters: fixed missing semi colon

--- src/Metrics/Characters.java
+++ src/Metrics/Characters.java
@@ -8,10 +8,12 @@ public class Characters {
     public int characterCount(String fileContent) throws MetricsException {
         try {
             int chars;
-            if (fileContent.isEmpty()) throws MetricsException
+            if (fileContent.isEmpty()) throws MetricsException();
             chars = fileContent.length();
             return chars + 1;
-        } catch(MetricsException e)
+        } catch(MetricsException e) {
+
+        }
     }
 
 }
