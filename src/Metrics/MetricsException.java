14d8d8e5345ff262a39157b2a7028513e60336be
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:26:19 -0800

    MetricsException: used super with param

--- src/Metrics/MetricsException.java
+++ src/Metrics/MetricsException.java
@@ -3,7 +3,9 @@ package Metrics;
 //Safety measures for null/empty strings
 public class MetricsException extends Exception{
 
+    public MetricsException() {}
 
     public MetricsException(String exceptionMsg){
+        super(exceptionMsg);
     }
 }
