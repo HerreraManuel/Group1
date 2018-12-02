3ad6d37a6d80adec9fe67f21735ccd40e45b2f34
HerreraManuel <mherrera@csus.edu>
2018-12-02 00:50:51 -0800

    SourceLine: used asList and split fileContent

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -7,7 +7,7 @@ public class SourceLine {
 
     public int sourceLine(String fileContent){
         ArrayList<String> lines;
-        lines = new ArrayList<String>(Arrays.asList(fileContent));
+        lines = new ArrayList<String>(Arrays.asList(fileContent.split("\n")));
         String[] lineKeeper;
         lineKeeper = fileContent.split(("\\r?\\n"));
         return 0;
