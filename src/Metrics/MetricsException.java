25d3deae4c0c204f9334b127bb3c3d018115df9e
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:18:35 -0800

    MetricsException: extends Exception class

--- src/Metrics/MetricsException.java
+++ src/Metrics/MetricsException.java
@@ -1,5 +1,6 @@
 package Metrics;
 
-public class MetricsException {
-    //Safety measures for null/empty strings
+//Safety measures for null/empty strings
+public class MetricsException extends Exception{
+
 }
