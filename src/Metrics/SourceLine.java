42cb4a23bea430ae0f36b9f283fe7a71f702baaa
HerreraManuel <mherrera@csus.edu>
2018-12-02 00:17:13 -0800

    SourceLine: Source Lines of Code integer

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -5,6 +5,7 @@ public class SourceLine {
     public int sourceLine(String fileContent){
         String[] lineKeeper;
         lineKeeper = fileContent.split(("\\r?\\n"));
+        int sloc = 0;
         return 0;
     }
 
