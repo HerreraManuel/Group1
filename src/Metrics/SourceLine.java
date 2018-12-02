e63bd38041fc2c1192c24785e0f2e883ebad6895
HerreraManuel <mherrera@csus.edu>
2018-12-02 01:02:48 -0800

    SourceLine: for-loop for ArrayList parse

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -9,6 +9,9 @@ public class SourceLine {
         ArrayList<String> lines;
         lines = new ArrayList<String>(Arrays.asList(fileContent.split("\n")));
         int sloc = 0, cloc = 0, bloc = 0;
+        for (int i = 0; i < lines.size(); i++){
+
+        }
         return 0;
     }
 
