85cffd53f78256b191cdbd2392bafff149c9934b
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:31:56 -0800

    Characters: calling MetricsException

--- src/Metrics/Characters.java
+++ src/Metrics/Characters.java
@@ -1,12 +1,17 @@
 package Metrics;
+import com.sun.javafx.font.Metrics;
+
 import java.io.*;
 
 public class Characters {
 
-    public int characterCount(String fileContent) {
-        int chars;
-        chars = fileContent.length();
-        return chars + 1;
+    public int characterCount(String fileContent) throws MetricsException {
+        try {
+            int chars;
+            if (fileContent.isEmpty()) throws MetricsException
+            chars = fileContent.length();
+            return chars + 1;
+        } catch(MetricsException e)
     }
 
 }
