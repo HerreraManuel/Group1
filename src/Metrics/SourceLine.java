b8f4dbb32fa8b6f8ecca847e9942ac94eabc3843
HerreraManuel <mherrera@csus.edu>
2018-12-02 02:14:19 -0800

    SourceLine: return correct variable, sloc.

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SourceLine {
-
     public int sourceLine(String fileContent) {
         ArrayList<String> lines;
         lines = new ArrayList<String>(Arrays.asList(fileContent.split("\n")));
@@ -26,6 +25,6 @@ public class SourceLine {
             sloc++;
         }
         sloc = sloc - (cloc + bloc);
-        return 0;
+        return sloc;
     }
 }
