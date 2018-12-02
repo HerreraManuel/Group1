999ffae3dc9bc49ee2e2c36de0aa0306827393b2
HerreraManuel <mherrera@csus.edu>
2018-12-02 00:26:20 -0800

    SourceLine: Create ArrayList<String>

--- src/Metrics/SourceLine.java
+++ src/Metrics/SourceLine.java
@@ -3,6 +3,7 @@ package Metrics;
 public class SourceLine {
 
     public int sourceLine(String fileContent){
+        ArrayList<String> lines;
         String[] lineKeeper;
         lineKeeper = fileContent.split(("\\r?\\n"))
         return 0;
